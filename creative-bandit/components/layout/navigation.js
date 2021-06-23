a2d8d9d999d100d8b3aebeb516ea89744b6299b9
Adam Speer <adamspeer@gmail.com>
2021-06-22 21:03:46 -0400

    added style changes and fixes

--- creative-bandit/components/layout/navigation.js
+++ creative-bandit/components/layout/navigation.js
@@ -2,16 +2,32 @@ import Link from 'next/link';
 import styles from '../../styles/Navigation.module.css';
 
 export default function Navigation() {
-    return (
-        <div classList={styles.navContainer}>
-            <nav classList={styles.nav}>
-                <ul classList={styles.ul}>
-                    <li classList={styles.li}><Link href='' classList={styles.link}>Home</Link></li>
-                    <li classList={styles.li}><Link href='' classList={styles.link}>Portfolio</Link></li>
-                    <li classList={styles.li}><Link href='' classList={styles.link}>Resume</Link></li>
-                    <li classList={styles.li}><Link href='' classList={styles.link}>Contact</Link></li>
-                </ul>
-            </nav>
-        </div>
-    ); 
+  return (
+    <div className={styles.navContainer}>
+      <nav className={styles.nav}>
+        <ul className={styles.ul}>
+          <li className={styles.li}>
+            <Link href='/' className={styles.link}>
+              Home
+            </Link>
+          </li>
+          <li className={styles.li}>
+            <Link href='/portfolio' className={styles.link}>
+              Portfolio
+            </Link>
+          </li>
+          <li className={styles.li}>
+            <Link href='/' className={styles.link}>
+              Resume
+            </Link>
+          </li>
+          <li className={styles.li}>
+            <Link href='/' className={styles.link}>
+              Contact
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
 }
