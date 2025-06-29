760c581d7d30c5602590e707687d0f6fe0059532
Adam <adamspeer@gmail.com>
2025-06-29 09:45:05 -0400

    initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,13 @@
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import mdx from '@astrojs/mdx';
+
+export default defineConfig({
+  integrations: [tailwind(), mdx()],
+  markdown: {
+    shikiConfig: {
+      theme: 'dracula',
+      wrap: true
+    }
+  }
+});
